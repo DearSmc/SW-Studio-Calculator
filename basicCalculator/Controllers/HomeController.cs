8dd9139dcf05594302a33cbcff06aa41ac2b2639
Dear <dear19052543@gmail.com>
2022-04-14 19:54:22 +0700

    final commit

---
+++ basicCalculator/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using basicCalculator.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [Produces("text/html")]
+        public IActionResult Calculate(string number)
+        {
+            CalculatorModel cal = new CalculatorModel();
+            string value = new DataTable().Compute(number, null).ToString();
+            cal.CalculateValue = value;
+            return Content(cal.CalculateValue, "text/html", Encoding.UTF8);
+        }
+    }
+}
---
+++ basicCalculator/Models/CalculatorModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace basicCalculator.Models
+{
+    public class CalculatorModel
+    {
+        public string CalculateValue { get; set; }
+    }
+}
